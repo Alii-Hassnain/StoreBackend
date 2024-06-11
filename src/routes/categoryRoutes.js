9ea04eefd9bddbed4fb5d3c35754fbe972f6f657
Alii-Hassnain <tohassnain@gmail.com>
2024-06-11 21:21:18 +0500

    importing upload to the routes

--- src/routes/categoryRoutes.js
+++ src/routes/categoryRoutes.js
@@ -6,7 +6,6 @@ import {
 } from "../controllers/categoryController.js";
 
 const router = express.Router();
-
 router.post("/categories", createCategory);
 router.get("/categories", getCategory);
 
--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -2,6 +2,32 @@ import express from "express";
 
 import { createProduct, getProduct } from "../controllers/productController.js";
 
+import { upload } from "../middleware/multer.js";
+
+
+// router.route("/register").post(
+//     upload.fields([
+//         {
+//             name: "avatar",
+//             maxCount: 1
+//         }, 
+//         {
+//             name: "coverImage",
+//             maxCount: 1
+//         }
+//     ]),
+//     registerUser
+//     )
+
 const router = express.Router();
-router.post("/products", createProduct);
+
+router.route("/products").post(
+    upload.fields([
+        {
+            name: "image",
+            maxCount: 1
+        }
+    ]),
+    createProduct
+)
 router.get("/products", getProduct);
